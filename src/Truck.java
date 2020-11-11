679ef9f997ae0aa883cc7332362d5b1933721e2c
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-11 09:49:36 -0500

    create the second branch

---
+++ src/Truck.java
@@ -0,0 +1,15 @@
+public class Truck {
+
+    String model;
+    public Truck (String model) {
+        this.model = model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
