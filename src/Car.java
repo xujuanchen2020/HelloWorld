5fbd93d37b50ada6f0d671d98965170edb844660
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-11 09:03:15 -0500

    Initial commit Intellij Test

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    String model;
+    public Car (String model) {
+        this.model = model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+}
